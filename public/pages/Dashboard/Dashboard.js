9f0b46335608bb3d39aaddc6bba300ab0f83a8c9
Emily Guo <emilyguo@amazon.com>
2024-06-25 10:41:11 -0700

    Add 2 tabs to home screen
    Signed-off-by: Emily Guo <emilyguo@amazon.com>

---
+++ public/pages/Dashboard/Dashboard.js
@@ -0,0 +1,21 @@
+import { Component } from "react";
+
+export default class Dashboard extends Component{
+    constructor(props) {
+        super(props);
+    }
+  
+    // Define the component's methods here
+    myMethod() {
+      // Method implementation
+    }
+  
+    render() {
+      // Return the component's JSX or DOM structure
+      return (
+        <div>
+          {/* Component JSX */}
+        </div>
+      );
+    }
+  }
