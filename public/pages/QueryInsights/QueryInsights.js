ec6ae90a00bd205bc54d92caedeee03777a71889
Emily Guo <emilyguo@amazon.com>
2024-07-03 16:34:49 -0700

    search in general
    Signed-off-by: Emily Guo <emilyguo@amazon.com>

--- public/pages/QueryInsights/QueryInsights.js
+++ public/pages/QueryInsights/QueryInsights.js
@@ -1,13 +1,15 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { EuiBasicTable,  EuiFlexItem, EuiFieldSearch, EuiSuperDatePicker, EuiFlexGroup, EuiSpacer } from '@elastic/eui';
-// import testItems from './test';
+import React, { useState, useEffect } from 'react';
+import { EuiBasicTable,  EuiFlexItem, EuiFieldSearch, EuiSuperDatePicker, EuiFlexGroup, EuiSpacer, EuiInMemoryTable, CriteriaWithPagination } from '@elastic/eui';
 
 const QueryInsights = () => {
 
   useEffect(() => {
     return () => {
+      onRefresh({ start, end } )
     };
   }, []);
+
+  // Generated fake queries
   const testItems = [
     {
       "timestamp" : 1719871061174,
@@ -88,7 +90,7 @@ const QueryInsights = () => {
         "fetch" : 0
       },
       "node_id" : "F9Qw4La8RiOZXji7s6WtTw",
-      "total_shards" : 1,
+      "total_shards" : 2,
       "latency" : 17,
       "memory" : 818120,
       "cpu" : 6625000
@@ -119,7 +121,7 @@ const QueryInsights = () => {
       ],
       "source" : "{\"size\":20,\"query\":{\"term\":{\"user.id\":{\"value\":\"cyji\",\"boost\":1.0}}}}",
       "labels" : { },
-      "search_type" : "query_then_fetch",
+      "search_type" : "fetch",
       "indices" : [
         "my-index-0"
       ],
@@ -177,7 +179,7 @@ const QueryInsights = () => {
       ],
       "phase_latency_map" : { },
       "node_id" : "F9Qw4La8RiOZXji7s6WtTw",
-      "total_shards" : 1,
+      "total_shards" : 5,
       "latency" : 2,
       "memory" : 133360,
       "cpu" : 1610000
@@ -202,46 +204,56 @@ const QueryInsights = () => {
     }
   ];
 
+  const [queries, setQueries] = useState(testItems);
+
   const convertTimestamp = (unixTime) => {
     const date = new Date(unixTime);
     const loc = date.toDateString().split(' ');
     return loc[1] + ' ' + loc[2] + ', ' + loc[3] + " @ " + date.toLocaleTimeString("en-US");
   };
+
   const cols = [
     {
+      field: 'timestamp',
       name: 'Time stamp',
-      render: (item) => convertTimestamp(item.timestamp),
-      sortable: (item) => item.timestamp,
+      render: (timestamp) => convertTimestamp(timestamp),
+      sortable: true,
       truncateText: true,
     },
     {
+      field: 'latency',
       name: 'Latency',
-      render: (item) => `${item.latency} ms`,
-      sortable: (item) => item.latency,
+      render: (latency) => `${latency} ms`,
+      sortable: true,
       truncateText: true,
     },
     {
+      field: 'cpu',
       name: 'CPU usage',
-      render: (item) => `${item.cpu} ns`,
-      sortable: (item) => item.cpu,
+      render: (cpu) => `${cpu} ns`,
+      sortable: true,
       truncateText: true,
     },
     {
+      field: 'memory',
       name: 'Memory',
-      render: (item) => `${item.memory} B`,
-      sortable: (item) => item.memory,
+      render: (memory) => `${memory} B`,
+      sortable: true,
       truncateText: true,
     },
     {
       field: 'indices',
       name: 'Indices',
+      render: (indices) => indices.toString(),
       sortable: true,
       truncateText: true,
+      // render: (indices) =>
     },
     {
+      field: 'search_type',
       name: 'Search type',
-      render: (item) => item.search_type.replaceAll('_', ' '),
-      sortable: (item) => item.search_type,
+      render: (search_type) => search_type.replaceAll('_', ' '),
+      sortable: true,
       truncateText: true,
     },
     {
@@ -257,39 +269,16 @@ const QueryInsights = () => {
       truncateText: true,
     },
   ];
-  // const items = [
-  //   {
-  //     "timestamp" : 1719871061255,
-  //     "node_id" : "qaZrSOygTjmu2C9P8yw9AQ",
-  //     "total_shards" : 1,
-  //     "phase_latency_map" : {
-  //       "expand" : 1,
-  //       "query" : 2,
-  //       "fetch" : 10
-  //     },
-  //     "search_type" : "query_then_fetch",
-  //     "indices" : [
-  //       "my-index-0"
-  //     ],
-  //     "source" : "{\"query\":{\"range\":{\"user.age\":{\"from\":50,\"to\":null,\"include_lower\":false,\"include_upper\":true,\"boost\":1.0}}}}",
-  //     "latency" : 14,
-  //     "cpu": 240,
-  //     "memory": 30,
-  //   }
-  // ];
-  const [sortField, setSortField] = useState('timestamp');
-  const [sortDirection, setSortDirection] = useState('asc');
+
   const sorting = {
     sort: {
-      field: sortField,
-      direction: sortDirection,
-    },
-    enableAllColumns: true,
-  };
-  const onTableChange = ({ sort = {} }) => {
-    const { field: sortField, direction: sortDirection } = sort;
-    setSortField(sortField);
-    setSortDirection(sortDirection);
+      field: 'timestamp',
+      direction: 'desc',
+    }
+  }
+
+  const onTableChange = (nextValues) => {
+    console.log(nextValues);
   };
 
   const [recentlyUsedRanges, setRecentlyUsedRanges] = useState([{ start: 'now-30m', end: 'now' }]);
@@ -313,41 +302,63 @@ const QueryInsights = () => {
     setStart(start);
     setEnd(end);
     setRecentlyUsedRanges(usedRange.length > 5 ? usedRange.slice(0, 5) : usedRange);
-    setLoading(true);
-    startLoading();
+    setQueries(testItems.filter((item) => item.timestamp >= start && item.timestamp <= end));
   };
 
-  const onRefresh = ({ start, end, refreshInterval }) => {
-    return new Promise((resolve) => {
-      setTimeout(resolve, 100);
-    }).then(() => {
-      console.log(start, end, refreshInterval);
-    });
+  const onRefresh = async ({ start, end }) => {
+    // setLoading(true);
+    setQueries(testItems.filter((item) => item.timestamp >= start && item.timestamp <= end));
+    // return new Promise((resolve) => {
+    //   setTimeout(resolve, 100);
+    // }).then(() => {
+    //   console.log(start, end, refreshInterval);
+    // });
+  };
+
+  const searchTopNQueries = () => {
+    return {
+      box: {
+        placeholder: 'Search queries',
+        schema: true,
+      },
+      // filters:
+      //   [
+      //     {
+      //       type: 'field_value_selection',
+      //         field: 'indices',
+      //       name: 'Index',
+      //       multiSelect: false,
+      //       options: store.countries.map((country) => ({
+      //       value: country.code,
+      //       name: country.name,
+      //       view: `${country.flag} ${country.name}`,
+      //     })),
+      //     },
+      //   ],
+      toolsRight: [
+        <EuiSuperDatePicker
+          recentlyUsedRanges={recentlyUsedRanges}
+          isLoading={loading}
+          start={start}
+          end={end}
+          onTimeChange={onTimeChange}
+          onRefresh={onRefresh}
+          updateButtonProps={{ fill: false }}
+        />,
+      ],
+    };
   };
 
   return (
     <div>
-      <EuiFlexGroup>
-        <EuiFlexItem>
-          <EuiFieldSearch fullWidth placeholder="Search query" />
-        </EuiFlexItem>
-        <EuiFlexItem grow={false} style={{ justifyContent: 'flex-end' }}>
-          <EuiSuperDatePicker
-            recentlyUsedRanges={recentlyUsedRanges}
-            isLoading={loading}
-            start={start}
-            end={end}
-            onTimeChange={onTimeChange}
-            onRefresh={onRefresh}
-          />
-        </EuiFlexItem>
-      </EuiFlexGroup>
-      <EuiSpacer size={'l'} direction={'vertical'}/>
-      <EuiBasicTable
-        items={testItems}
+      <EuiInMemoryTable
+        items={queries}
         columns={cols}
         sorting={sorting}
-        onChange={onTableChange}
+        onTableChange={onTableChange}
+        loading={loading}
+        search={searchTopNQueries()}
+        allowNeutralSort={false}
       />
     </div>
   );
