5e9ad96ac36a03a33ee298a6646607eafeb683a5
Emily Guo <emilyguo@amazon.com>
2024-07-11 14:22:20 -0700

    More cleanups
    Signed-off-by: Emily Guo <emilyguo@amazon.com>

--- public/application.js
+++ public/application.js
@@ -1,20 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { AppMountParameters, CoreStart } from '../../../src/core/public';
-// import { AppPluginStartDependencies } from './types';
 import { QueryInsightsDashboardsApp } from './components/app';
 import { HashRouter as Router, Route } from 'react-router-dom';
-// import { CoreContext } from './utils/CoreContext';
-// import { ServicesContext, NotificationService, getDataSourceEnabled } from './services';
-// import { initManageChannelsUrl } from './utils/helpers';
 
 export const renderApp = (
-  // { notifications, http }: CoreStart,
   coreStart,
   { navigation },
   { appBasePath, element }
 ) => {
-  // const isDarkMode = coreStart.uiSettings.get('theme:darkMode') || false;
   coreStart.chrome.setBreadcrumbs([{text: 'Query insights'}]);
   ReactDOM.render(
     <Router>
