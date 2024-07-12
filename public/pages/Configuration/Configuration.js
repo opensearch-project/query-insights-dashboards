ea6ad658e57a2a8209463d99e345ac1fbb94221d
Emily Guo <emilyguo@amazon.com>
2024-07-12 16:27:36 -0700

    Fixing lint and including relevant files
    Signed-off-by: Emily Guo <emilyguo@amazon.com>

--- public/pages/Configuration/Configuration.js
+++ public/pages/Configuration/Configuration.js
@@ -1,14 +1,22 @@
-import React, { useEffect, useState } from 'react';
-import { EuiFlexItem, EuiPanel, EuiTitle, EuiFlexGrid, EuiText, EuiFieldNumber, EuiSelect, EuiFlexGroup, EuiFormRow, EuiForm, EuiButton, EuiButtonEmpty, EuiBottomBar } from '@elastic/eui';
+import React, { useState } from 'react';
+import {
+  EuiFlexItem,
+  EuiPanel,
+  EuiTitle,
+  EuiFlexGrid,
+  EuiText,
+  EuiFieldNumber,
+  EuiSelect,
+  EuiFlexGroup,
+  EuiFormRow,
+  EuiForm,
+  EuiButton,
+  EuiButtonEmpty,
+  EuiBottomBar,
+} from '@elastic/eui';
 import { useHistory } from 'react-router-dom';
 
 const Configuration = () => {
-
-  useEffect(() => {
-    return () => {
-    };
-  }, []);
-
   const timeUnits = [
     { value: 'MINUTES', text: 'Minute(s)' },
     { value: 'HOURS', text: 'Hour(s)' },
@@ -41,7 +49,7 @@ const Configuration = () => {
 
   const onTimeChange = (e) => {
     setTime(e.target.value);
-  }
+  };
 
   const MinutesBox = () => (
     <EuiSelect
@@ -63,9 +71,14 @@ const Configuration = () => {
     />
   );
 
-  let WindowChoice = (time === timeUnits[0].value) ? MinutesBox : HoursBox;
+  const WindowChoice = time === timeUnits[0].value ? MinutesBox : HoursBox;
 
-  let changed = (topNSize !== defaultTopN) ? 'topN': (windowSize !== defaultWindowSize) ? 'windowSize' : null;
+  let changed;
+  if (topNSize !== defaultTopN) {
+    changed = 'topN';
+  } else if (windowSize !== defaultWindowSize) {
+    changed = 'windowSize';
+  }
 
   let valid = false;
   if (1 <= topNSize && topNSize <= 100) {
@@ -87,14 +100,12 @@ const Configuration = () => {
   return (
     <div>
       <EuiFlexItem grow={false} style={{ width: '60%' }}>
-        <EuiPanel style={{ padding: '20px 20px' }} >
+        <EuiPanel style={{ padding: '20px 20px' }}>
           <EuiForm>
             <EuiFlexItem>
               <EuiTitle size="s">
                 <EuiText size="s">
-                  <h2>
-                    Latency settings
-                  </h2>
+                  <h2>Latency settings</h2>
                 </EuiText>
               </EuiTitle>
             </EuiFlexItem>
@@ -102,16 +113,19 @@ const Configuration = () => {
               <EuiFlexGrid columns={2} gutterSize="s" style={{ padding: '15px 0px' }}>
                 <EuiFlexItem style={{ padding: '0px 30px 0px 0px' }}>
                   <EuiText size="xs">
-                    <h3>
-                      Value of N (count)
-                    </h3>
+                    <h3>Value of N (count)</h3>
                   </EuiText>
                   <EuiText size="xs" style={{ lineHeight: '22px', padding: '5px 0px' }}>
-                    Specify the value of N. N is the number of queries to be collected with in the window size.
+                    Specify the value of N. N is the number of queries to be collected with in the
+                    window size.
                   </EuiText>
                 </EuiFlexItem>
                 <EuiFlexItem>
-                  <EuiFormRow label="latency.top_n_size" helpText="Max allowed limit 100." style={{ padding: '0px 0px 20px' }}>
+                  <EuiFormRow
+                    label="latency.top_n_size"
+                    helpText="Max allowed limit 100."
+                    style={{ padding: '0px 0px 20px' }}
+                  >
                     <EuiFlexItem>
                       <EuiFieldNumber
                         min={1}
@@ -125,19 +139,21 @@ const Configuration = () => {
                 </EuiFlexItem>
                 <EuiFlexItem style={{ padding: '0px 30px 0px 0px' }}>
                   <EuiText size="xs">
-                    <h3>
-                      Window size
-                    </h3>
+                    <h3>Window size</h3>
                   </EuiText>
                   <EuiText size="xs" style={{ lineHeight: '22px', padding: '5px 0px' }}>
                     The duration during which the Top N queries are collected.
                   </EuiText>
                 </EuiFlexItem>
                 <EuiFlexItem>
-                  <EuiFormRow label="latency.window_size" helpText="Max allowed limit 24 hours." style={{ padding: '15px 0px 5px' }}>
+                  <EuiFormRow
+                    label="latency.window_size"
+                    helpText="Max allowed limit 24 hours."
+                    style={{ padding: '15px 0px 5px' }}
+                  >
                     <EuiFlexGroup>
                       <EuiFlexItem style={{ flexDirection: 'row' }}>
-                        <WindowChoice/>
+                        <WindowChoice />
                       </EuiFlexItem>
                       <EuiFlexItem>
                         <EuiSelect
@@ -156,21 +172,22 @@ const Configuration = () => {
           </EuiForm>
         </EuiPanel>
       </EuiFlexItem>
-      {(changed && valid) ? (
+      {changed && valid ? (
         <EuiBottomBar>
           <EuiFlexGroup gutterSize="s" justifyContent="flexEnd">
             <EuiFlexItem grow={false}>
-              <EuiButtonEmpty
-                color="ghost"
-                size="s"
-                iconType="cross"
-                onClick={reset}
-              >
+              <EuiButtonEmpty color="ghost" size="s" iconType="cross" onClick={reset}>
                 Cancel
               </EuiButtonEmpty>
             </EuiFlexItem>
             <EuiFlexItem grow={false}>
-              <EuiButton color="primary" fill size="s" iconType="check" onClick={() => history.push('/queryInsights')}>
+              <EuiButton
+                color="primary"
+                fill
+                size="s"
+                iconType="check"
+                onClick={() => history.push('/queryInsights')}
+              >
                 Save
               </EuiButton>
             </EuiFlexItem>
@@ -182,5 +199,3 @@ const Configuration = () => {
 };
 
 export default Configuration;
-
-
--- public/pages/TopNQueries/TopNQueries.js
+++ public/pages/TopNQueries/TopNQueries.js
@@ -1,15 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import { useHistory, useLocation, Switch, Route, Redirect } from 'react-router-dom';
+import React from 'react';
+import { useHistory, useLocation, Switch, Route } from 'react-router-dom';
 import { EuiTab, EuiTabs, EuiTitle } from '@elastic/eui';
 import Configuration from '../Configuration/Configuration';
-import QueryInsights from '../QueryInsights/QueryInsights';
 import { FormattedMessage } from '@osd/i18n/react';
 
-const TopNQueries = (props) => {
-  const conf = 'configuration'
+const TopNQueries = () => {
   const history = useHistory();
   const location = useLocation();
-  const [selectedTabId, setSelectedTabId] = useState(location.pathname.includes(conf) ? conf : 'topNQueries')
 
   const tabs = [
     {
@@ -23,15 +20,6 @@ const TopNQueries = (props) => {
       route: 'configuration',
     },
   ];
-
-  useEffect(() => {
-    const { pathname: prevPathname } = location;
-    const { pathname: currPathname } = props.location;
-    if (prevPathname !== currPathname) {
-      const selectedTabId = currPathname.includes(conf) ? conf : 'topNQueries';
-      setSelectedTabId(selectedTabId);
-    }
-  }, [location, props.location]);
 
   const onSelectedTabChanged = (route) => {
     const { pathname: currPathname } = location;
@@ -56,13 +44,13 @@ const TopNQueries = (props) => {
         <Route
           exact
           path="/queryInsights"
-          render={(props) => (
+          render={() => (
             <EuiTitle size="l">
               <h1>
                 <FormattedMessage
-                  id={"queryInsightsDashboards.topnqueries"}
+                  id={'queryInsightsDashboards.topnqueries'}
                   defaultMessage="{name}"
-                  values={{ name: "Query insights - Top N queries" }}
+                  values={{ name: 'Query insights - Top N queries' }}
                 />
               </h1>
             </EuiTitle>
@@ -71,13 +59,13 @@ const TopNQueries = (props) => {
         <Route
           exact
           path="/configuration"
-          render={(props) => (
+          render={() => (
             <EuiTitle size="l">
               <h1>
                 <FormattedMessage
-                  id={"queryInsightsDashboards.configuration"}
+                  id={'queryInsightsDashboards.configuration'}
                   defaultMessage="{name}"
-                  values={{ name: "Query insights - Configuration" }}
+                  values={{ name: 'Query insights - Configuration' }}
                 />
               </h1>
             </EuiTitle>
@@ -89,25 +77,7 @@ const TopNQueries = (props) => {
       </div>
       <div>
         <Switch>
-          <Route
-            exact
-            path="/queryInsights"
-            render={(props) => (
-              <QueryInsights
-                {...props}
-              />
-            )}
-          />
-          <Route
-            exact
-            path="/configuration"
-            render={(props) => (
-              <Configuration
-                {...props}
-              />
-            )}
-          />
-          <Redirect to="/queryInsights" />
+          <Route exact path="/configuration" render={(props) => <Configuration {...props} />} />
         </Switch>
       </div>
     </div>
