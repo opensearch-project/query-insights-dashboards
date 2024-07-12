c7b92e6e749742c5fdf81c6ed958f294a9bb6237
Emily Guo <emilyguo@amazon.com>
2024-07-12 16:31:17 -0700

    Unused import
    Signed-off-by: Emily Guo <emilyguo@amazon.com>

--- public/components/app.js
+++ public/components/app.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, BrowserRouter as Router } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import TopNQueries from '../pages/TopNQueries/TopNQueries'
 
 export const QueryInsightsDashboardsApp = ({props}) => {
