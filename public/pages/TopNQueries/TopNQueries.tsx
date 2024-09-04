d943c3ea1469806d919bde05569fbdc4883ef0aa
Emily Guo <LilyCaroline1717@gmail.com>
2024-09-04 19:13:47 -0400

    Connected api to frontend
    Signed-off-by: Emily Guo <LilyCaroline1717@gmail.com>

--- public/pages/TopNQueries/TopNQueries.tsx
+++ public/pages/TopNQueries/TopNQueries.tsx
@@ -1,6 +1,7 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import { Redirect, Route, Switch, useHistory, useLocation } from 'react-router-dom';
 import { EuiTab, EuiTabs, EuiTitle, EuiSpacer } from '@elastic/eui';
+import dateMath from '@elastic/datemath';
 import QueryInsights from '../QueryInsights/QueryInsights';
 import Configuration from '../Configuration/Configuration';
 import { CoreStart } from '../../../../../src/core/public';
@@ -91,18 +92,63 @@ const TopNQueries = ({ core }: { core: CoreStart }) => {
     </EuiTab>
   );
 
-  const retrieveQueries = useCallback(async (start: string, end: string) => {
-    try {
-      setLoading(true);
-      const noDuplicates: any[] = [];
-      setQueries(noDuplicates);
-    } catch (error) {
-      // eslint-disable-next-line no-console
-      console.error('Error retrieving queries:', error);
-    } finally {
-      setLoading(false);
-    }
-  }, []);
+  const parseDateString = (dateString: string) => {
+    const date = dateMath.parse(dateString);
+    return date ? date.toDate().getTime() : new Date().getTime();
+  };
+
+  const retrieveQueries = useCallback(
+    async (start: string, end: string) => {
+      const nullResponse = { response: { top_queries: [] } };
+      const params = {
+        query: {
+          from: new Date(parseDateString(start)).toISOString(),
+          to: new Date(parseDateString(end)).toISOString(),
+        },
+      };
+      const fetchMetric = async (endpoint: string) => {
+        try {
+          const response = await core.http.get(endpoint, params);
+          return {
+            response: {
+              top_queries: Array.isArray(response?.response?.top_queries)
+                ? response.response.top_queries
+                : [],
+            },
+          };
+        } catch {
+          return nullResponse;
+        }
+      };
+      try {
+        setLoading(true);
+        const respLatency = latencySettings.isEnabled
+          ? await fetchMetric('/api/top_queries/latency')
+          : nullResponse;
+        const respCpu = cpuSettings.isEnabled
+          ? await fetchMetric('/api/top_queries/cpu')
+          : nullResponse;
+        const respMemory = memorySettings.isEnabled
+          ? await fetchMetric('/api/top_queries/memory')
+          : nullResponse;
+        const newQueries = [
+          ...respLatency.response.top_queries,
+          ...respCpu.response.top_queries,
+          ...respMemory.response.top_queries,
+        ];
+        const noDuplicates = Array.from(
+          new Set(newQueries.map((item) => JSON.stringify(item)))
+        ).map((item) => JSON.parse(item));
+        setQueries(noDuplicates);
+      } catch (error) {
+        // eslint-disable-next-line no-console
+        console.error('Error retrieving queries:', error);
+      } finally {
+        setLoading(false);
+      }
+    },
+    [latencySettings, cpuSettings, memorySettings, core]
+  );
 
   const retrieveConfigInfo = useCallback(
     async (
@@ -113,19 +159,67 @@ const TopNQueries = ({ core }: { core: CoreStart }) => {
       newWindowSize: string = '',
       newTimeUnit: string = ''
     ) => {
-      try {
-        setMetricSettings(metric, {
-          isEnabled: enabled,
-          currTopN: newTopN,
-          currWindowSize: newWindowSize,
-          currTimeUnit: newTimeUnit,
-        });
-      } catch (error) {
-        // eslint-disable-next-line no-console
-        console.error('Failed to set settings:', error);
+      if (get) {
+        try {
+          const resp = await core.http.get('/api/settings');
+          const settings = resp.response.persistent.search.insights.top_queries;
+          const latency = settings.latency;
+          const cpu = settings.cpu;
+          const memory = settings.memory;
+          if (latency !== undefined && latency.enabled === 'true') {
+            const [time, timeUnits] = latency.window_size.match(/\D+|\d+/g);
+            setMetricSettings('latency', {
+              isEnabled: true,
+              currTopN: latency.top_n_size,
+              currWindowSize: time,
+              currTimeUnit: timeUnits === 'm' ? 'MINUTES' : 'HOURS',
+            });
+          }
+          if (cpu !== undefined && cpu.enabled === 'true') {
+            const [time, timeUnits] = cpu.window_size.match(/\D+|\d+/g);
+            setMetricSettings('cpu', {
+              isEnabled: true,
+              currTopN: cpu.top_n_size,
+              currWindowSize: time,
+              currTimeUnit: timeUnits === 'm' ? 'MINUTES' : 'HOURS',
+            });
+          }
+          if (memory !== undefined && memory.enabled === 'true') {
+            const [time, timeUnits] = memory.window_size.match(/\D+|\d+/g);
+            setMetricSettings('memory', {
+              isEnabled: true,
+              currTopN: memory.top_n_size,
+              currWindowSize: time,
+              currTimeUnit: timeUnits === 'm' ? 'MINUTES' : 'HOURS',
+            });
+          }
+        } catch (error) {
+          // eslint-disable-next-line no-console
+          console.error('Failed to retrieve settings:', error);
+        }
+      } else {
+        try {
+          setMetricSettings(metric, {
+            isEnabled: enabled,
+            currTopN: newTopN,
+            currWindowSize: newWindowSize,
+            currTimeUnit: newTimeUnit,
+          });
+          await core.http.put('/api/update_settings', {
+            query: {
+              metric,
+              enabled,
+              top_n_size: newTopN,
+              window_size: `${newWindowSize}${newTimeUnit === 'MINUTES' ? 'm' : 'h'}`,
+            },
+          });
+        } catch (error) {
+          // eslint-disable-next-line no-console
+          console.error('Failed to set settings:', error);
+        }
       }
     },
-    []
+    [core]
   );
 
   const onTimeChange = ({ start, end }: { start: string; end: string }) => {
@@ -136,6 +230,7 @@ const TopNQueries = ({ core }: { core: CoreStart }) => {
     setStart(start);
     setEnd(end);
     setRecentlyUsedRanges(usedRange.length > 10 ? usedRange.slice(0, 9) : usedRange);
+    retrieveConfigInfo(true);
     retrieveQueries(start, end);
   };
 
@@ -146,7 +241,7 @@ const TopNQueries = ({ core }: { core: CoreStart }) => {
 
   useEffect(() => {
     retrieveQueries(currStart, currEnd);
-  }, [currStart, currEnd, retrieveQueries]);
+  }, [latencySettings, cpuSettings, memorySettings, currStart, currEnd, retrieveQueries]);
 
   return (
     <div style={{ padding: '35px 35px' }}>
--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,17 +1,215 @@
+import { schema } from '@osd/config-schema';
 import { IRouter } from '../../../../src/core/server';
-
 export function defineRoutes(router: IRouter) {
   router.get(
     {
-      path: '/api/query_insights_dashboards/example',
+      path: '/api/top_queries',
       validate: false,
     },
     async (context, request, response) => {
-      return response.ok({
-        body: {
-          time: new Date().toISOString(),
-        },
-      });
+      try {
+        const client = context.queryInsights_plugin.queryInsightsClient.asScoped(request)
+          .callAsCurrentUser;
+        const res = await client('queryInsights.getTopNQueries');
+        return response.custom({
+          statusCode: 200,
+          body: {
+            ok: true,
+            response: res,
+          },
+        });
+      } catch (error) {
+        console.error('Unable to get top queries: ', error);
+        return response.ok({
+          body: {
+            ok: false,
+            response: error.message,
+          },
+        });
+      }
+    }
+  );
+
+  router.get(
+    {
+      path: '/api/top_queries/latency',
+      validate: {
+        query: schema.object({
+          from: schema.maybe(schema.string({ defaultValue: '' })),
+          to: schema.maybe(schema.string({ defaultValue: '' })),
+        }),
+      },
+    },
+    async (context, request, response) => {
+      try {
+        const { from, to } = request.query;
+        const params = { from, to };
+        const client = context.queryInsights_plugin.queryInsightsClient.asScoped(request)
+          .callAsCurrentUser;
+        const res = await client('queryInsights.getTopNQueriesLatency', params);
+        return response.custom({
+          statusCode: 200,
+          body: {
+            ok: true,
+            response: res,
+          },
+        });
+      } catch (error) {
+        console.error('Unable to get top queries (latency): ', error);
+        return response.ok({
+          body: {
+            ok: false,
+            response: error.message,
+          },
+        });
+      }
+    }
+  );
+
+  router.get(
+    {
+      path: '/api/top_queries/cpu',
+      validate: {
+        query: schema.object({
+          from: schema.maybe(schema.string({ defaultValue: '' })),
+          to: schema.maybe(schema.string({ defaultValue: '' })),
+        }),
+      },
+    },
+    async (context, request, response) => {
+      try {
+        const { from, to } = request.query;
+        const params = { from, to };
+        const client = context.queryInsights_plugin.queryInsightsClient.asScoped(request)
+          .callAsCurrentUser;
+        const res = await client('queryInsights.getTopNQueriesCpu', params);
+        return response.custom({
+          statusCode: 200,
+          body: {
+            ok: true,
+            response: res,
+          },
+        });
+      } catch (error) {
+        console.error('Unable to get top queries (cpu): ', error);
+        return response.ok({
+          body: {
+            ok: false,
+            response: error.message,
+          },
+        });
+      }
+    }
+  );
+
+  router.get(
+    {
+      path: '/api/top_queries/memory',
+      validate: {
+        query: schema.object({
+          from: schema.maybe(schema.string({ defaultValue: '' })),
+          to: schema.maybe(schema.string({ defaultValue: '' })),
+        }),
+      },
+    },
+    async (context, request, response) => {
+      try {
+        const { from, to } = request.query;
+        const params = { from, to };
+        const client = context.queryInsights_plugin.queryInsightsClient.asScoped(request)
+          .callAsCurrentUser;
+        const res = await client('queryInsights.getTopNQueriesMemory', params);
+        return response.custom({
+          statusCode: 200,
+          body: {
+            ok: true,
+            response: res,
+          },
+        });
+      } catch (error) {
+        console.error('Unable to get top queries (memory): ', error);
+        return response.ok({
+          body: {
+            ok: false,
+            response: error.message,
+          },
+        });
+      }
+    }
+  );
+
+  router.get(
+    {
+      path: '/api/settings',
+      validate: false,
+    },
+    async (context, request, response) => {
+      try {
+        const client = context.queryInsights_plugin.queryInsightsClient.asScoped(request)
+          .callAsCurrentUser;
+        const res = await client('queryInsights.getSettings');
+        return response.custom({
+          statusCode: 200,
+          body: {
+            ok: true,
+            response: res,
+          },
+        });
+      } catch (error) {
+        console.error('Unable to get top queries: ', error);
+        return response.ok({
+          body: {
+            ok: false,
+            response: error.message,
+          },
+        });
+      }
+    }
+  );
+
+  router.put(
+    {
+      path: '/api/update_settings',
+      validate: {
+        query: schema.object({
+          metric: schema.maybe(schema.string({ defaultValue: '' })),
+          enabled: schema.maybe(schema.boolean({ defaultValue: false })),
+          top_n_size: schema.maybe(schema.string({ defaultValue: '' })),
+          window_size: schema.maybe(schema.string({ defaultValue: '' })),
+        }),
+      },
+    },
+    async (context, request, response) => {
+      try {
+        const query = request.query;
+        const client = context.queryInsights_plugin.queryInsightsClient.asScoped(request)
+          .callAsCurrentUser;
+        const params = {
+          body: {
+            persistent: {
+              [`search.insights.top_queries.${query.metric}.enabled`]: query.enabled,
+              [`search.insights.top_queries.${query.metric}.top_n_size`]: query.top_n_size,
+              [`search.insights.top_queries.${query.metric}.window_size`]: query.window_size,
+            },
+          },
+        };
+        const res = await client('queryInsights.setSettings', params);
+        return response.custom({
+          statusCode: 200,
+          body: {
+            ok: true,
+            response: res,
+          },
+        });
+      } catch (error) {
+        console.error('Unable to set settings: ', error);
+        return response.ok({
+          body: {
+            ok: false,
+            response: error.message,
+          },
+        });
+      }
     }
   );
 }
