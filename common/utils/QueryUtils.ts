9b87bc0a5d1b4ccaa0737b0f4edc88a83428cacf
Kishore Kumaar Natarajan <kkumaarn@amazon.com>
2025-06-20 12:25:34 -0700

    FIx-query-details-verbose-param
    Signed-off-by: Kishore Kumaar Natarajan <kkumaarn@amazon.com>

--- common/utils/QueryUtils.ts
+++ common/utils/QueryUtils.ts
@@ -21,7 +21,8 @@ export const retrieveQueryById = async (
   dataSourceId: string,
   start: string | null,
   end: string | null,
-  id: string | null
+  id: string | null,
+  verbose: boolean
 ): Promise<SearchQueryRecord | null> => {
   const nullResponse = { response: { top_queries: [] } };
   const params = {
@@ -30,6 +31,7 @@ export const retrieveQueryById = async (
       from: start,
       to: end,
       id,
+      verbose,
     },
   };
 
