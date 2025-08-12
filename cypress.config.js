5a58c8adb86d3dc50aafe38c482b452be9cc5afd
Chenyang Ji <cyji@amazon.com>
2025-08-12 13:58:20 -0700

    Fix failed cypress tests and fix table sort in 2.19 (#320)
    Signed-off-by: Chenyang Ji <cyji@amazon.com>
    Signed-off-by: Jason Chenyang Ji <chenyang.yale@gmail.com>

--- cypress.config.js
+++ cypress.config.js
@@ -6,6 +6,15 @@ module.exports = defineConfig({
   defaultCommandTimeout: 60000,
   requestTimeout: 600000,
   responseTimeout: 600000,
+  pageLoadTimeout: 120000,
+  // Increase timeouts for CI environments
+  taskTimeout: 120000,
+  execTimeout: 120000,
+  // Retry configuration for flaky tests
+  retries: {
+    runMode: 2,
+    openMode: 0
+  },
   env: {
     openSearchUrl: 'http://localhost:9200',
     SECURITY_ENABLED: false,
@@ -16,8 +25,17 @@ module.exports = defineConfig({
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
+      // Increase timeouts for CI environments
+      if (config.isInteractive === false) {
+        config.defaultCommandTimeout = 90000;
+        config.requestTimeout = 90000;
+        config.responseTimeout = 90000;
+        config.pageLoadTimeout = 180000;
+      }
       return require('./cypress/plugins/index.js')(on, config)
     },
     baseUrl: 'http://localhost:5601',
+    // Slow down tests slightly to reduce race conditions
+    slowTestThreshold: 30000,
   },
 })
