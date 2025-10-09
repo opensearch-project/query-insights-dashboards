a915127bea30ca5c065982dbe2758324868e3e99
Lingxi Chen <lingxich@amazon.com>
2025-10-09 00:58:04 -0700

    adding unit tests
    Signed-off-by: Lingxi Chen <lingxich@amazon.com>

--- public/pages/WorkloadManagement/WLMDetails/WLMDetails.tsx
+++ public/pages/WorkloadManagement/WLMDetails/WLMDetails.tsx
@@ -123,6 +123,12 @@ interface Rule {
   username: string;
 }
 
+interface DataSourceAttributes {
+  title: string;
+  dataSourceVersion?: string; // 👈 the field we want
+  // ...others the DS plugin stores
+}
+
 // === Main Component ===
 export const WLMDetails = ({
   core,
